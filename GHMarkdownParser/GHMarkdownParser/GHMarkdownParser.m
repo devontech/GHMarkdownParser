4ce39192d42656e850e47e83c89bbe7681fad1a3
Eric Böhnisch-Volkmann <eboehnisch@devontechnologies.com>
2015-12-22 09:37:23 +0100

    Use explicit casts

--- GHMarkdownParser/GHMarkdownParser/GHMarkdownParser.m
+++ GHMarkdownParser/GHMarkdownParser/GHMarkdownParser.m
@@ -15,7 +15,7 @@
     NSString *HTMLString = nil;
     
     char *markdownUTF8 = (char*)markdownString.UTF8String;
-    Document *document = mkd_string(markdownUTF8, strlen(markdownUTF8), 0);
+    Document *document = mkd_string(markdownUTF8, (int)strlen(markdownUTF8), 0);
     if (document && mkd_compile(document, 0)) {
         char *HTMLUTF8 = NULL;
         int length = mkd_document(document, &HTMLUTF8);
